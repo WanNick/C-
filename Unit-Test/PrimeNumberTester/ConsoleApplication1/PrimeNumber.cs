339750669e064c2ffc88f9c0253dd81e9ca09fb0
WanNick <nickwan@hotmail.co.uk>
2015-10-25 15:03:25 +0000

    This is PrimeTestertest

---
+++ Unit-Test/PrimeNumberTester/ConsoleApplication1/PrimeNumber.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    public class PrimeNumber
+    {
+        public bool isPrime(int input, int input2)
+        {
+
+            int a = 0;
+
+            for (int i = 1; i <= input; i++)
+            {
+                if (input % i == 0)
+                {
+                    a++;
+                }
+            }
+            if (a == 2)
+            {
+                return true;
+            }
+
+            if (input <= 0)
+                // The IF statement above checks to see whether input is less than or equal to zero.
+                // if it's less than or equal to 0 it will return true. 
+            {
+                return true;
+            }
+            if (input > 0)
+                // The IF statement above checks to see whether input2 is greater than zero.
+                // if it is its going to return false.
+            {
+                return false;
+            }
+
+            return false;
+
+            //throw new NotImplementedException();
+        }
+    }
+}
---
+++ Unit-Test/PrimeNumberTester/PrimeNumberTester/UnitTest1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PrimeNumberTester
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void IsThisNumberPrime()
+        {
+            //ARRANGE
+            var checker = new ConsoleApplication1.PrimeNumber();
+
+            // ACT
+            var result = checker.isPrime(17, 17);
+            // Change the first number within the bracket to check to see if it's a Prime. i.e change the number (##, 17).
+
+            //ASSERT
+
+            Assert.AreEqual(true, result);
+        }
+
+        [TestMethod]
+        public void IsNumberLessThanZero()
+        {
+            // ARRANGE
+
+            var checker = new ConsoleApplication1.PrimeNumber();
+
+            // ACT
+            var result = checker.isPrime(-3, -3);
+            //Change the first value i.e (-3, 0); Change the first (-3) to test if number is greater than zero.
+
+            // ASSERT
+            Assert.AreEqual(true, result);
+
+        }
+        [TestMethod]
+        public void IsNumberGreaterThanZero()
+        {
+            // ARRANGE
+
+            var checker = new ConsoleApplication1.PrimeNumber();
+
+            // ACT
+            var result = checker.isPrime(68, 0);
+            //Change the first value i.e (1, 0); Change the one to test if number is greater than zero.
+
+            // ASSERT
+            Assert.AreEqual(false, result);
+        }
+    }
+}
