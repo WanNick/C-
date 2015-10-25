325ca1aeec43193f3cade06681366c71040b8bd8
WanNick <nickwan@hotmail.co.uk>
2015-10-25 13:01:30 +0000

    This is the Hello World tester.

---
+++ HelloWorldtest/HelloWorldtest/UnitTest1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HelloWorldtest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void HelloWorldTester()
+        {
+            // ARRANGE
+            var checker = new helloWorld1.helloWorldTester();
+            // ACT
+            var result = checker.helloWorld("Hello World", "Hello World");
+
+            // ASSERT
+            Assert.AreEqual(true, result);
+        }
+        [TestMethod]
+        public void TestIfStringIsDifferent()
+        {
+            // ACT
+            var check = new helloWorld1.helloWorldTester();
+
+            // ARRANGE
+
+            var result = check.helloWorld("Hello World", "");
+
+            // ASSERT
+            Assert.AreEqual(true, result);
+        }
+        [TestMethod]
+        public void TestIfStringIsEmpty()
+        {
+            // ARRANGE
+            var check = new helloWorld1.helloWorldTester();
+
+            //ACT
+            var result = check.helloWorld(string.Empty, string.Empty);
+            //ASSERT
+
+            Assert.AreEqual(true, result);
+           
+        }
+        [TestMethod]
+        public void TestIfStringContainsUpperCaseH()
+        {
+            // ARRANGE
+            var check = new helloWorld1.helloWorldTester();
+
+            // ACT
+            var result = check.helloWorld("Hello World", "");
+
+            // ASSERT
+            Assert.AreEqual(true, result);
+        }
+    }
+}
---
+++ HelloWorldtest/helloWorld1/helloWorldTester.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace helloWorld1
+{
+    public class helloWorldTester
+    {
+        public bool helloWorld(string input, string input1)
+        {
+            input1 = "";
+            
+            if (input == "Hello World")
+            {
+                return true;
+            }
+            if (input != input1)
+            {
+                return false;
+            }
+            if (input == string.Empty)
+            {
+                return true;
+            }
+            if (input1 == string.Empty)
+            {
+                return true;
+            }
+
+            if (input.Contains("H"))
+            {
+                return true;
+            }
+            return false;
+
+            //throw new NotImplementedException();
+        }
+    }
+}
