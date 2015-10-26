3c7825e992366e7d648764b0896b9d5f02687898
WanNick <nw406@student.anglia.ac.uk>
2015-10-26 19:12:04 +0000

    New Experiment Console App with Unit Tests.

---
+++ Unit-Test/Experiment/Experiment/Add.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Experiment
+{
+    class Add
+    {
+        public static void addition()
+        {
+            Console.WriteLine("This selection allows you to find the sum of two numbers \n");
+            Console.WriteLine("Please enter the first number: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.Clear();
+            Console.WriteLine("Please enter the second number: ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            Console.Clear();
+            int ans = num1 + num2;
+
+            Console.WriteLine("The sum of the {0} and {1} is {2}", num1, num2, ans);
+            Console.ReadLine();
+
+        }
+    }
+}
